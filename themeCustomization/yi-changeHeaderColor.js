69d78900c9387a3ef1439b36e51db5531063d048
yoshinobu5 <39183246+yoshinobu5@users.noreply.github.com>
2018-08-01 00:37:03 +0900

    Update yi-changeHeaderColor.js

--- themeCustomization/yi-changeHeaderColor.js
+++ themeCustomization/yi-changeHeaderColor.js
@@ -16,7 +16,21 @@ document.addEventListener('DOMContentLoaded', function(){
 　console.log("yi-changeHeaderColor: elt_nav_bar_include=" + elt_nav_bar_include);
 
   if (elt_nav_bar_include != null) {
-    elt_nav_bar_include.setAttribute("style", style_text);      
+    elt_nav_bar_include.setAttribute("style", style_text);
+    
+    var mo = new MutationObserver(function(records){
+      
+      var elt_children = elt_nav_bar_include.children;
+      for (var i = 0; i < elt_children.length; i++) {
+        console.log("yi-changeHeaderColor: elt_children[" + i + "]=" + elt_children[i].tagName);
+        elt_children[i].setAttribute("style", style_text);
+
+        if (elt_children[i].tagName == 'A') {
+             mo.disconnect();
+        }
+      }
+    });
+    mo.observe(elt_nav_bar_include, {childList: true, subtree: true});
   }
   else {
     console.log("yi-changeHeaderColor: document.URL=" + document.URL);
@@ -24,9 +38,7 @@ document.addEventListener('DOMContentLoaded', function(){
       console.log("yi-changeHeaderColor: /mycontants is true.");
       var elt_lotusBanner = document.getElementById('lotusBanner');
       if (elt_lotusBanner != null) {
-
         var mo = new MutationObserver(function(records){
-
           elt_nav_bar_include = document.getElementById('nav_bar_include');
           console.log("yi-changeHeaderColor: MutationObserver event is fired. [if(document.URL=/myconnect)] elt_nav_bar_include=(" + elt_nav_bar_include + ")");
           elt_nav_bar_include.setAttribute("style", style_text);
@@ -49,7 +61,6 @@ document.addEventListener('DOMContentLoaded', function(){
       var elt_ocsBanner = document.getElementById('ocsBanner');
       if (elt_ocsBanner != null) {
         var mo = new MutationObserver(function(records){
-          
           elt_nav_bar_include = document.getElementById('nav_bar_include');
           console.log("yi-changeHeaderColor: MutationObserver event is fired. [else] elt_nav_bar_include=(" + elt_nav_bar_include + ")");
           elt_nav_bar_include.setAttribute("style", style_text);
