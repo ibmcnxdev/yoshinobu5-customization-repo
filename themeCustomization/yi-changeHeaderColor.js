2f23bf0680967ffb88ef102636c95e8bda0643ee
yoshinobu5 <39183246+yoshinobu5@users.noreply.github.com>
2018-07-31 16:23:11 +0900

    Update yi-changeHeaderColor.js

--- themeCustomization/yi-changeHeaderColor.js
+++ themeCustomization/yi-changeHeaderColor.js
@@ -6,36 +6,47 @@
 // ==/UserScript==
 
 document.addEventListener('DOMContentLoaded', function(){  
+  console.log("yi-changeHeaderColor: Event is fired. document.URL=" + document.URL);
+  if (new RegExp("https://apps.ap.collabserv.com").test(document.URL) || new RegExp("https://apps.na.collabserv.com").test(document.URL) || new RegExp("https://apps.ce.collabserv.com").test(document.URL)) {
+    
+//  var style_text = "background-color: #000;"
+  var style_text = "color: #ff0000 !important; background-color: #000;"
 
   var elt_nav_bar_include = document.getElementById('nav_bar_include');
-//　console.log("elt_nav_bar_include=" + elt_nav_bar_include);
+　console.log("yi-changeHeaderColor: elt_nav_bar_include=" + elt_nav_bar_include);
 
   if (elt_nav_bar_include != null) {
-    elt_nav_bar_include.setAttribute("style", "background-color: #000;");      
+    elt_nav_bar_include.setAttribute("style", style_text);      
   }
   else {
-//    console.log("document.URL=" + document.URL);
+    console.log("yi-changeHeaderColor: document.URL=" + document.URL);
     if (new RegExp("/mycontacts").test(document.URL)) {
+      console.log("yi-changeHeaderColor: /mycontants is true.");
       var elt_lotusBanner = document.getElementById('lotusBanner');
       if (elt_lotusBanner != null) {
         var mo = new MutationObserver(records => {
-          elt_lotusBanner = document.getElementById('nav_bar_include');
-          elt_lotusBanner.setAttribute("style", "background-color: #000;");
+          elt_nav_bar_include = document.getElementById('nav_bar_include');
+          console.log("yi-changeHeaderColor: MutationObserver event is fired. [if(document.URL=/myconnect)] elt_nav_bar_include=(" + elt_nav_bar_include + ")");
+          elt_nav_bar_include.setAttribute("style", style_text);
           mo.disconnect();
         });
         mo.observe(elt_lotusBanner, {childList: true});
       }
     }
     else {
+      console.log("yi-changeHeaderColor: /mycontants is false.");
       var elt_ocsBanner = document.getElementById('ocsBanner');
       if (elt_ocsBanner != null) {
         var mo = new MutationObserver(records => {
           elt_nav_bar_include = document.getElementById('nav_bar_include');
-          elt_nav_bar_include.setAttribute("style", "background-color: #000;");
+          console.log("yi-changeHeaderColor: MutationObserver event is fired. [else] elt_nav_bar_include=(" + elt_nav_bar_include + ")");
+          elt_nav_bar_include.setAttribute("style", style_text);
           mo.disconnect();
         });
         mo.observe(elt_ocsBanner, {childList: true});    
       }
     }
   }
+
+  }  
 });
