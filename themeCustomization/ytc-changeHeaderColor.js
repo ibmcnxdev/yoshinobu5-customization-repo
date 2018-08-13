fc54de28133f990c122c2c56fd5cab77a55edea3
yoshinobu5 <39183246+yoshinobu5@users.noreply.github.com>
2018-08-13 21:24:50 +0900

    Update ytc-changeHeaderColor.js

--- themeCustomization/ytc-changeHeaderColor.js
+++ themeCustomization/ytc-changeHeaderColor.js
@@ -6,13 +6,13 @@
 // ==/UserScript==
 
 document.addEventListener('DOMContentLoaded', function(){  
- console.log("yi-changeHeaderColor: Event is fired. document.URL=" + document.URL);
+ console.log("ytc-changeHeaderColor: Event is fired. document.URL=" + document.URL);
  if (new RegExp("https://apps.ap.collabserv.com").test(document.URL) && new RegExp("https://apps.na.collabserv.com").test(document.URL)) {
 
   var style_text = "background-color: #3e8e74;";
 
   var elt_nav_bar_include = document.getElementById('nav_bar_include');
-　console.log("yi-changeHeaderColor: elt_nav_bar_include=" + elt_nav_bar_include);
+  console.log("ytc-changeHeaderColor: elt_nav_bar_include=" + elt_nav_bar_include);
 
   if (elt_nav_bar_include != null) {
     elt_nav_bar_include.setAttribute("style", style_text);
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function(){
       
       var elt_children = elt_nav_bar_include.children;
       for (var i = 0; i < elt_children.length; i++) {
-        console.log("yi-changeHeaderColor: elt_children[" + i + "]=" + elt_children[i].tagName);
+        console.log("ytc-changeHeaderColor: elt_children[" + i + "]=" + elt_children[i].tagName);
         elt_children[i].setAttribute("style", style_text);
 
         if (i==0 && (elt_children[i].tagName == 'A' || elt_children[i].tagName == 'IMG')) {
@@ -32,19 +32,19 @@ document.addEventListener('DOMContentLoaded', function(){
     mo.observe(elt_nav_bar_include, {childList: true, subtree: true});
   }
   else {
-    console.log("yi-changeHeaderColor: document.URL=" + document.URL);
+    console.log("ytc-changeHeaderColor: document.URL=" + document.URL);
     if (new RegExp("/mycontacts").test(document.URL)) {
-      console.log("yi-changeHeaderColor: /mycontants is true.");
+      console.log("ytc-changeHeaderColor: /mycontants is true.");
       var elt_lotusBanner = document.getElementById('lotusBanner');
       if (elt_lotusBanner != null) {
         var mo = new MutationObserver(function(records){
           elt_nav_bar_include = document.getElementById('nav_bar_include');
-          console.log("yi-changeHeaderColor: MutationObserver event is fired. [if(document.URL=/myconnect)] elt_nav_bar_include=(" + elt_nav_bar_include + ")");
+          console.log("ytc-changeHeaderColor: MutationObserver event is fired. [if(document.URL=/myconnect)] elt_nav_bar_include=(" + elt_nav_bar_include + ")");
           elt_nav_bar_include.setAttribute("style", style_text);
           
           var elt_children = elt_nav_bar_include.children;
           for (var i = 0; i < elt_children.length; i++) {
-            console.log("yi-changeHeaderColor: elt_children[" + i + "]=" + elt_children[i].tagName);
+            console.log("ytc-changeHeaderColor: elt_children[" + i + "]=" + elt_children[i].tagName);
             elt_children[i].setAttribute("style", style_text);
             
             if (i==0 && (elt_children[i].tagName == 'A' || elt_children[i].tagName == 'IMG')) {
@@ -56,18 +56,18 @@ document.addEventListener('DOMContentLoaded', function(){
       }
     }
     else {
-      console.log("yi-changeHeaderColor: /mycontants is false.");
+      console.log("ytc-changeHeaderColor: /mycontants is false.");
       var elt_ocsBanner = document.getElementById('ocsBanner');
       if (elt_ocsBanner != null) {
         var mo = new MutationObserver(function(records){
           elt_nav_bar_include = document.getElementById('nav_bar_include');
-          console.log("yi-changeHeaderColor: MutationObserver event is fired. [else] elt_nav_bar_include=(" + elt_nav_bar_include + ")");
+          console.log("ytc-changeHeaderColor: MutationObserver event is fired. [else] elt_nav_bar_include=(" + elt_nav_bar_include + ")");
           elt_nav_bar_include.setAttribute("style", style_text);
           
           var elt_children = elt_nav_bar_include.children;
-          console.log("yi-changeHeaderColor: elt_nav_bar_include.children.length=" + elt_nav_bar_include.children.length);
+          console.log("ytc-changeHeaderColor: elt_nav_bar_include.children.length=" + elt_nav_bar_include.children.length);
           for (var i = 0; i < elt_children.length; i++) {
-            console.log("yi-changeHeaderColor: elt_children[" + i + "]=" + elt_children[i].tagName);
+            console.log("ytc-changeHeaderColor: elt_children[" + i + "]=" + elt_children[i].tagName);
             elt_children[i].setAttribute("style", style_text);
             
             if (i==0 && (elt_children[i].tagName == 'A' || elt_children[i].tagName == 'IMG')) {
