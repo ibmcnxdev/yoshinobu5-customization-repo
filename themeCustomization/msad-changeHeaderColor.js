c7a72d9e2615db8e118ffcd597392280b4e2727c
yoshinobu5 <39183246+yoshinobu5@users.noreply.github.com>
2019-01-09 20:30:27 +0900

    Update msad-changeHeaderColor.js

--- themeCustomization/msad-changeHeaderColor.js
+++ themeCustomization/msad-changeHeaderColor.js
@@ -1,18 +1,18 @@
 // ==UserScript==
-// @name     yi-changeHeaderColor
+// @name     msad-changeHeaderColor
 // @description Change of Connections Cloud's color.
 // @version  1
 // @grant    none
 // ==/UserScript==
 
 document.addEventListener('DOMContentLoaded', function(){  
- console.log("yi-changeHeaderColor: Event is fired. document.URL=" + document.URL);
+ console.log("msad-changeHeaderColor: Event is fired. document.URL=" + document.URL);
  if (new RegExp("https://apps.ap.collabserv.com").test(document.URL) || new RegExp("https://apps.na.collabserv.com").test(document.URL)) {
 
   var style_text = "background-color: #2e675a;"
 
   var elt_nav_bar_include = document.getElementById('nav_bar_include');
-　console.log("yi-changeHeaderColor: elt_nav_bar_include=" + elt_nav_bar_include);
+　console.log("msad-changeHeaderColor: elt_nav_bar_include=" + elt_nav_bar_include);
 
   if (elt_nav_bar_include != null) {
 // Old code
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function(){
 
 // New code
       var style_of_elt_nav_bar_include = elt_nav_bar_include.getAttribute("style");
-						console.log("yi-changeHeaderColor: elt_nav_bar_include@style (Before)=" + style_of_elt_nav_bar_include);
+						console.log("msad-changeHeaderColor: elt_nav_bar_include@style (Before)=" + style_of_elt_nav_bar_include);
       if (style_of_elt_nav_bar_include == null) {
          elt_nav_bar_include.setAttribute("style", style_text);
 						}
@@ -30,20 +30,20 @@ document.addEventListener('DOMContentLoaded', function(){
 						else {
          elt_nav_bar_include.setAttribute("style", style_of_elt_nav_bar_include + ' ' + style_text);
 						}
-      console.log("yi-changeHeaderColor: elt_nav_bar_include@style (After)=" + elt_nav_bar_include.getAttribute("style")); 
+      console.log("msad-changeHeaderColor: elt_nav_bar_include@style (After)=" + elt_nav_bar_include.getAttribute("style")); 
 // -----
 
     var mo = new MutationObserver(function(records){
       
       var elt_children = elt_nav_bar_include.children;
       for (var i = 0; i < elt_children.length; i++) {
-        console.log("yi-changeHeaderColor: elt_children[" + i + "]=" + elt_children[i].tagName);
+        console.log("msad-changeHeaderColor: elt_children[" + i + "]=" + elt_children[i].tagName);
 // Old code
 //      elt_children[i].setAttribute("style", style_text);
 
 // New code
 						  var style_of_elt_children = elt_children[i].getAttribute("style");
-        console.log("yi-changeHeaderColor: elt_children[" + i + "]@style (Before)=" + style_of_elt_children);
+        console.log("msad-changeHeaderColor: elt_children[" + i + "]@style (Before)=" + style_of_elt_children);
         if (style_of_elt_children == null) {
            elt_children[i].setAttribute("style", style_text);
 						  }
@@ -53,7 +53,7 @@ document.addEventListener('DOMContentLoaded', function(){
 						  else {
            elt_children[i].setAttribute("style", style_of_elt_children + ' ' + style_text);
 						  }
-        console.log("yi-changeHeaderColor: elt_children[" + i + "]@style (After)=" + elt_children[i].getAttribute("style"));
+        console.log("msad-changeHeaderColor: elt_children[" + i + "]@style (After)=" + elt_children[i].getAttribute("style"));
 // -----
 
         if (i==0 && (elt_children[i].tagName == 'A' || elt_children[i].tagName == 'IMG')) {
@@ -64,19 +64,19 @@ document.addEventListener('DOMContentLoaded', function(){
     mo.observe(elt_nav_bar_include, {childList: true, subtree: true});
   }
   else {
-    console.log("yi-changeHeaderColor: document.URL=" + document.URL);
+    console.log("msad-changeHeaderColor: document.URL=" + document.URL);
     if (new RegExp("/mycontacts").test(document.URL)) {
-      console.log("yi-changeHeaderColor: /mycontants is true.");
+      console.log("msad-changeHeaderColor: /mycontants is true.");
       var elt_lotusBanner = document.getElementById('lotusBanner');
       if (elt_lotusBanner != null) {
         var mo = new MutationObserver(function(records){
           elt_nav_bar_include = document.getElementById('nav_bar_include');
-          console.log("yi-changeHeaderColor: MutationObserver event is fired. [if(document.URL=/myconnect)] elt_nav_bar_include=(" + elt_nav_bar_include + ")");
+          console.log("msad-changeHeaderColor: MutationObserver event is fired. [if(document.URL=/myconnect)] elt_nav_bar_include=(" + elt_nav_bar_include + ")");
 // Old code
 //        elt_nav_bar_include.setAttribute("style", style_text);
 // New code
           var style_of_elt_nav_bar_include = elt_nav_bar_include.getAttribute("style");
-						    console.log("yi-changeHeaderColor: elt_nav_bar_include@style (Before)=" + style_of_elt_nav_bar_include);
+						    console.log("msad-changeHeaderColor: elt_nav_bar_include@style (Before)=" + style_of_elt_nav_bar_include);
           if (style_of_elt_nav_bar_include == null) {
              elt_nav_bar_include.setAttribute("style", style_text);
 						    }
@@ -86,18 +86,18 @@ document.addEventListener('DOMContentLoaded', function(){
 						    else {
              elt_nav_bar_include.setAttribute("style", style_of_elt_nav_bar_include + ' ' + style_text);
 						    }
-          console.log("yi-changeHeaderColor: elt_nav_bar_include@style (After)=" + elt_nav_bar_include.getAttribute("style")); 
+          console.log("msad-changeHeaderColor: elt_nav_bar_include@style (After)=" + elt_nav_bar_include.getAttribute("style")); 
 // -----
 
           var elt_children = elt_nav_bar_include.children;
           for (var i = 0; i < elt_children.length; i++) {
-            console.log("yi-changeHeaderColor: elt_children[" + i + "]=" + elt_children[i].tagName);
+            console.log("msad-changeHeaderColor: elt_children[" + i + "]=" + elt_children[i].tagName);
 // Old code
 //          elt_children[i].setAttribute("style", style_text);
 
 // New code
 						      var style_of_elt_children = elt_children[i].getAttribute("style");
-            console.log("yi-changeHeaderColor: elt_children[" + i + "]@style (Before)=" + style_of_elt_children);
+            console.log("msad-changeHeaderColor: elt_children[" + i + "]@style (Before)=" + style_of_elt_children);
             if (style_of_elt_children == null) {
                elt_children[i].setAttribute("style", style_text);
 						      }
@@ -107,7 +107,7 @@ document.addEventListener('DOMContentLoaded', function(){
 						      else {
                elt_children[i].setAttribute("style", style_of_elt_children + ' ' + style_text);
 						      }
-            console.log("yi-changeHeaderColor: elt_children[" + i + "]@style (After)=" + elt_children[i].getAttribute("style"));
+            console.log("msad-changeHeaderColor: elt_children[" + i + "]@style (After)=" + elt_children[i].getAttribute("style"));
 // ------
 
             if (i==0 && (elt_children[i].tagName == 'A' || elt_children[i].tagName == 'IMG')) {
@@ -119,18 +119,18 @@ document.addEventListener('DOMContentLoaded', function(){
       }
     }
     else {
-      console.log("yi-changeHeaderColor: /mycontants is false.");
+      console.log("msad-changeHeaderColor: /mycontants is false.");
       var elt_ocsBanner = document.getElementById('ocsBanner');
       if (elt_ocsBanner != null) {
         var mo = new MutationObserver(function(records){
           elt_nav_bar_include = document.getElementById('nav_bar_include');
-          console.log("yi-changeHeaderColor: MutationObserver event is fired. [else] elt_nav_bar_include=(" + elt_nav_bar_include + ")");
+          console.log("msad-changeHeaderColor: MutationObserver event is fired. [else] elt_nav_bar_include=(" + elt_nav_bar_include + ")");
 // Old code
 //        elt_nav_bar_include.setAttribute("style", style_text);
 
 // New code
           var style_of_elt_nav_bar_include = elt_nav_bar_include.getAttribute("style");
-						    console.log("yi-changeHeaderColor: elt_nav_bar_include@style (Before)=" + style_of_elt_nav_bar_include);
+						    console.log("msad-changeHeaderColor: elt_nav_bar_include@style (Before)=" + style_of_elt_nav_bar_include);
           if (style_of_elt_nav_bar_include == null) {
              elt_nav_bar_include.setAttribute("style", style_text);
 						    }
@@ -140,19 +140,19 @@ document.addEventListener('DOMContentLoaded', function(){
 						    else {
              elt_nav_bar_include.setAttribute("style", style_of_elt_nav_bar_include + ' ' + style_text);
 						    }
-          console.log("yi-changeHeaderColor: elt_nav_bar_include@style (After)=" + elt_nav_bar_include.getAttribute("style")); 
+          console.log("msad-changeHeaderColor: elt_nav_bar_include@style (After)=" + elt_nav_bar_include.getAttribute("style")); 
 // -----
 
           var elt_children = elt_nav_bar_include.children;
-          console.log("yi-changeHeaderColor: elt_nav_bar_include.children.length=" + elt_nav_bar_include.children.length);
+          console.log("msad-changeHeaderColor: elt_nav_bar_include.children.length=" + elt_nav_bar_include.children.length);
           for (var i = 0; i < elt_children.length; i++) {
-            console.log("yi-changeHeaderColor: elt_children[" + i + "]=" + elt_children[i].tagName);
+            console.log("msad-changeHeaderColor: elt_children[" + i + "]=" + elt_children[i].tagName);
 // Old code
 //          elt_children[i].setAttribute("style", style_text);
 
 // New code
 						      var style_of_elt_children = elt_children[i].getAttribute("style");
-            console.log("yi-changeHeaderColor: elt_children[" + i + "]@style (Before)=" + style_of_elt_children);
+            console.log("msad-changeHeaderColor: elt_children[" + i + "]@style (Before)=" + style_of_elt_children);
             if (style_of_elt_children == null) {
                elt_children[i].setAttribute("style", style_text);
 						      }
@@ -162,7 +162,7 @@ document.addEventListener('DOMContentLoaded', function(){
 						      else {
                elt_children[i].setAttribute("style", style_of_elt_children + ' ' + style_text);
 						      }
-            console.log("yi-changeHeaderColor: elt_children[" + i + "]@style (After)=" + elt_children[i].getAttribute("style"));
+            console.log("msad-changeHeaderColor: elt_children[" + i + "]@style (After)=" + elt_children[i].getAttribute("style"));
 // ------
 
             if (i==0 && (elt_children[i].tagName == 'A' || elt_children[i].tagName == 'IMG')) {
