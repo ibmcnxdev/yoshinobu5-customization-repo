1eacede99a4be3cdbd57e6fb032f6491addb967b
yoshinobu5 <39183246+yoshinobu5@users.noreply.github.com>
2019-06-03 13:55:15 +0900

    Update controlCommunityExternalSharing2.js

--- controlExternalSharing/controlCommunityExternalSharing2.js
+++ controlExternalSharing/controlCommunityExternalSharing2.js
@@ -8,28 +8,34 @@
 //window.addEventListener('load', function(){
 document.addEventListener('DOMContentLoaded', function(){
   console.log("controlCommunityExternalSharing2: Event is fired. (document.URL=" + document.URL + ")");
+  var allowedExternalSharingURL = 'https://apps.ap.collabserv.com/files/form/api/library/ad0d984b-3282-4259-a9e8-5b6251d2f96a/document/5d28a198-cd58-43dd-aa66-2c2af25f9f89/media/userslist.txt';
 
   if (new RegExp("https://apps.ap.collabserv.com/communities/service/html/communitycreate").test(document.URL) ||
       new RegExp("https://apps.ce.collabserv.com/communities/service/html/communitycreate").test(document.URL) ||
       new RegExp("https://apps.na.collabserv.com/communities/service/html/communitycreate").test(document.URL)) {
 
     var elt_allowExternal = document.getElementById('allowExternal');
-    //console.log("elt_allowExternal=" + elt_allowExternal);
+    console.log("controlCommunityExternalSharing2: elt_allowExternal=" + elt_allowExternal);
     elt_allowExternal.checked=false;
     elt_allowExternal.setAttribute("disabled", "disabled");
     elt_allowExternal.parentElement.setAttribute("style", "display: none;");
 
+    var elt_advancedLink = document.getElementById('advancedLink');
+    console.log("controlCommunityExternalSharing2: elt_advancedLink=" + elt_advancedLink);
+    elt_advancedLink.setAttribute("style", "display: none;");
+    
     var allCookies = document.cookie.split(';');
-    //console.log("array_cookie=" + array_cookie);
+    console.log("controlCommunityExternalSharing2: array_cookie=" + array_cookie);
     var _ids = allCookies.filter(function(element, index, allCookies) {
-        return element.trim().startsWith("ids");
+//        return element.trim().match("^ids");
+          return new RegExp(/^ids/i).test(element.trim());
     });
-    //console.log("_ids=" + _ids);
+    console.log("controlCommunityExternalSharing2: _ids=" + _ids);
     var myUserID = (_ids[0].split('='))[1].split(':')[0];
     console.log("controlCommunityExternalSharing2: myUserID=" + myUserID);
 
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://apps.ap.collabserv.com/files/form/api/library/ad0d984b-3282-4259-a9e8-5b6251d2f96a/document/5d28a198-cd58-43dd-aa66-2c2af25f9f89/media/userslist.txt', true);
+    xhr.open('GET', allowedExternalSharingURL, true);
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
             var res_data = JSON.parse(xhr.responseText);
@@ -42,6 +48,9 @@ document.addEventListener('DOMContentLoaded', function(){
                 elt_allowExternal.removeAttribute("disabled");
                 elt_allowExternal.parentElement.setAttribute("style", "");
                 console.log("controlCommunityExternalSharing2: elt_allowExternal is displaying.");
+                
+                elt_advancedLink.setAttribute("style", "");
+                console.log("controlCommunityExternalSharing2: elt_advancedLink is displaying.");
             }
         }
     };
